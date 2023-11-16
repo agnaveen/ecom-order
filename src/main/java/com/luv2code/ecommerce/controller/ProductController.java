67bc259cbedc8e9da8e1c04df2d2bbbb9f0196e0
Naveen Gunasekaran <mail2agn@gmail.com>
2023-11-16 16:11:01 +0530

    cors config

--- src/main/java/com/luv2code/ecommerce/controller/ProductController.java
+++ src/main/java/com/luv2code/ecommerce/controller/ProductController.java
@@ -5,16 +5,14 @@ import com.luv2code.ecommerce.entity.products.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.PagedModel;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/products")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
     private final ProductsRepository productRepository;
